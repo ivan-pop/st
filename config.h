2a085b4ec53769e1e8d249280554df3ea7c834f7
Ivan Popovych <ivan.popovych@protonmail.com>
2020-03-04 11:06:51 +0200

    repo init

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "Liberation Mono:pixelsize=18:antialias=true:autohint=true";
+static char *font = "JetBrains Mono Medium:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 0;
 
 /*
@@ -168,7 +168,7 @@ static unsigned int defaultrcs = 0;
  * 6: Bar ("|")
  * 7: Snowman ("☃")
  */
-static unsigned int cursorshape = 2;
+static unsigned int cursorshape = 6;
 
 /*
  * Default columns and rows numbers
